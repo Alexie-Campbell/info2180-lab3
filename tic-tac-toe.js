04bf4552f941af6fd8f13a7da45ce4fdd110b83c
Alexie-Campbell <alexie.campbell@mymona.uwi.edu>
2022-10-13 22:14:01 -0500

    Excercise 2 complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,12 +8,36 @@ window.onload= function(){
   for (var i = 0; i < board.children.length; i++) {
       var square = board.children[i];
       if (square.tagName == 'DIV') {
-            square.setAttribute("class", "square")
+            square.setAttribute("class", "square");
+            square.addEventListener("click", squareClicked);
       }
   }
 }
 
+const playBoard = [];
 
+function pushElement(el, object){
+  var element = el;
+  let thisSquare = object;
+  playBoard.push(element);
+    console.log(playBoard);
+    thisSquare.innerHTML = element;
+    thisSquare.classList.add(element);
+}
 
-
-
+function squareClicked(){
+  let thisSquare = this;
+  console.log("i clicked a square");
+  if (playBoard.length == 0){
+    pushElement("X",thisSquare);
+  }
+  else{
+    var lastPlayed = playBoard[playBoard.length-1];
+    if(lastPlayed == "X"){
+    pushElement("O", thisSquare);
+    }
+    else{
+     pushElement("X", thisSquare);
+    }
+  }
+}
