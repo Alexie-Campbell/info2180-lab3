5c3e3380a87ff5b3697aa174a52638c9f814e0d1
Alexie-Campbell <alexie.campbell@mymona.uwi.edu>
2022-10-14 18:00:47 -0500

    Excercise 4 Completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,14 @@
 "use strict";
 
 
-
 window.onload= function(){
   var board = document.getElementById('board');
   var classList = 'classList' in board;
-  for (var i = 0; i < board.children.length; i++) {
+  for (let i = 0; i < board.children.length; i++) {
       var square = board.children[i];
       if (square.tagName == 'DIV') {
             square.setAttribute("class", "square");
+            square.setAttribute("id", "pos"+(i+1));
             square.addEventListener("click", squareClicked);
             square.addEventListener("mouseover", squareHoverTrue);
             square.addEventListener("mouseout", squareHoverFalse);
@@ -17,14 +17,63 @@ window.onload= function(){
 }
 
 const playBoard = [];
+const playPositionsX = [];
+const playPositionsO = [];
+
+const winScenarios= [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']];
+//winScenarios include arrays of the possible wins (order does not matter)
+//for example array [1,2,3] would also work if the player played [3,2,1] or [2,3,1] or [1,3,2] etc
 
 function pushElement(el, object){
   var element = el;
   let thisSquare = object;
   playBoard.push(element);
-    console.log(playBoard);
-    thisSquare.innerHTML = element;
-    thisSquare.classList.add(element);
+  console.log(playBoard);
+  thisSquare.innerHTML = element;
+  thisSquare.classList.add(element);
+
+  var thisSquareID = thisSquare.id;
+  var thisSquarePos= thisSquareID.replace("pos","");
+  if (element == "X"){
+    playPositionsX.push(thisSquarePos);
+    console.log(playPositionsX);
+    var winX = checkSequence(playPositionsX);
+    console.log("successFlag"+winX);
+    if (winX){
+      announceWin("X");
+    }
+  }
+  else{
+    playPositionsO.push(thisSquarePos);
+    console.log(playPositionsO);
+    var winO = checkSequence(playPositionsO);
+    console.log("successFlag"+winO);
+    if (winO){
+      announceWin("O");
+    }
+  }
+}
+
+function announceWin(el){
+  let winner = el;
+  var status = document.getElementById("status");
+  status.classList.add("you-won");
+  status.innerHTML="Congratulations! "+winner+" is the Winner!";
+}
+
+function checkSequence(arrPos){
+  let arrayToCheck = arrPos;
+  console.log(arrayToCheck);
+  for (let i=0; i<winScenarios.length; i++){
+    let thisWinScenario = winScenarios[i];
+    console.log(thisWinScenario);
+    var successFlag = thisWinScenario.every((val) => arrayToCheck.includes(val));
+    console.log(successFlag);
+    if(successFlag){
+      break;
+    }
+  }
+  return successFlag;
 }
 
 function squareClicked(){
